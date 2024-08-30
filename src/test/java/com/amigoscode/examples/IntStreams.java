2cb844d7c2705e7212a7d9377eb5c0ee51b57127
hirenaway <hirenksolanki95@gmail.com>
2024-08-30 19:27:52 +0530

    IntStream commit

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -13,15 +13,35 @@ public class IntStreams {
 
     @Test
     public void range() throws Exception {
+        System.out.println("For i");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Exclusive");
+        IntStream.range(0,10).forEach(System.out::println);
+
+        System.out.println("Inclusive");
+        IntStream.rangeClosed(0,10).forEach(System.out::println);
+
     }
 
     // Loop through people using IntStream
     @Test
     public void rangeIteratingLists() throws Exception {
         List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    Person person = people.get(index);
+                    System.out.println(person);
+                });
     }
 
     @Test
     public void intStreamIterate()  {
+        IntStream.iterate(0, operand -> ++operand)
+                .filter(num -> num % 2 == 0) // printing only even numbers
+                .limit(20)
+                .forEach(System.out::println);
     }
 }
