3b1e6668bf7d622237fd407fdc0df296ad9af0ef
hirenaway <hirenksolanki95@gmail.com>
2024-08-31 23:40:05 +0530

    distinct commit

--- src/test/java/com/amigoscode/examples/DistinctAndSets.java
+++ src/test/java/com/amigoscode/examples/DistinctAndSets.java
@@ -14,10 +14,19 @@ public class DistinctAndSets {
     @Test
     public void distinct() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+
+        // if you want a List as a result use distinct
+        System.out.println(numbers.stream()
+                .distinct()
+                .collect(Collectors.toList()));
     }
 
     @Test
     public void distinctWithSet() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+
+        // if you want a Set as a result
+        System.out.println(numbers.stream()
+                .collect(Collectors.toSet()));
     }
 }
